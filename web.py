82c85631b0b7134f7fb4aea2a52ab4dc4984d2ba
Faqih Ardiansyah <faqihbackup2024@gmail.com>
2025-03-27 12:59:07 +0700

    Initial Commit Create Web App

---
+++ web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+st.title("My To-do App")
+st.subheader("Welcome to To-do List App")
+st.text("This App make your life easier")
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_to_do"] + "\n"
+    todos.append(todo)
+    functions.writes_todos(todos)
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.writes_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_to_do")
+
